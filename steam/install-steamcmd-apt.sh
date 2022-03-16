d554a2368a90db62ae8e526e3db5d95b6d179fe3
char00les <char00les@gmail.com>
2022-03-16 12:25:02 -0400

    Create install-steamcmd-apt.sh

---
+++ steam/install-steamcmd-apt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade
+sudo apt install software-properties-common
+sudo add-apt-repository multiverse
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install lib32gcc1 steamcmd

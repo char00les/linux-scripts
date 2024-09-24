851f53f133102f194015d11a2854ebd03d6b46a0
char00les <char00les@gmail.com>
2024-09-24 14:46:48 -0400

    Create cheatsheet.sh

---
+++ cheatsheet.sh
@@ -0,0 +1,6 @@
+## Detailed view of active network connections and listening ports
+# -t (TCP sockets)
+# -u (UDP sockets)
+# -l (listening sockets)
+# -n (numeric addresses without resolving hostnames).
+sudo ss -tuln
